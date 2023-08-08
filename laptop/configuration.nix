4769058d2dcb1a860bb5d1efb34774ed62f2ca98
Wolfram Friele <wolframfriele@gmail.com>
2023-08-08 09:15:15 +0200

    Fix adding xfce

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -38,22 +38,22 @@
     };
   };
 
-  nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-    };
-  };
+  # nix = {
+  #   # This will add each flake input as a registry
+  #   # To make nix3 commands consistent with your flake
+  #   registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+  #
+  #   # This will additionally add your inputs to the system's legacy channels
+  #   # Making legacy nix commands consistent as well, awesome!
+  #   nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  #
+  #   settings = {
+  #     # Enable flakes and new 'nix' command
+  #     experimental-features = "nix-command flakes";
+  #     # Deduplicate and optimize nix store
+  #     auto-optimise-store = true;
+  #   };
+  # };
 
   # FIXME: Add the rest of your current configuration
 
@@ -111,8 +111,14 @@
   #     your-username = import ./home.nix;
   #   };
   # };
-
-  services.xserver.desktopManager.xfce.enable = true;
+  service.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+    displayManager.defaultSession = "xfce";
+  };
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   # services.openssh = {
