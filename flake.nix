967f41ca7001c333382b729164eddf4d0be8af73
Wolfram Friele <wolframfriele@gmail.com>
2023-08-08 14:38:26 +0200

    Attempt to fix wrong end of filew

--- flake.nix
+++ flake.nix
@@ -9,14 +9,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, hyprland, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -32,8 +30,6 @@
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
-          hyprland.homeManagerModules.default
-          {wayland.windowManager.hyprland.enable = true;}
         ];
       };
     };
