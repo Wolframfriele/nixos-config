57a8a43fd634a3f7975c1370ca1e698a62daeca9
Wolfram Friele <wolframfriele@gmail.com>
2023-08-14 21:31:50 +0200

    Separate out python

--- laptop/development/default.nix
+++ laptop/development/default.nix
@@ -1,22 +1,9 @@
 {pkgs, ...}: {
   imports = [
-    #./anyrun.nix
-  ];
-
-  home.packages = with pkgs; [
-    vscode
-
+    ./python.nix
   ];
   
-  let
-    base-python-packages = ps: with ps; [
-      pandas
-      seaborn
-      ipykernel
-      jupyter_client
-    ];
-  in
-  environment.systemPackages = [
-    (pkgs.python3.withPackages base-python-packages)
+  home.packages = with pkgs; [
+    vscode
   ];
 }
---
+++ laptop/development/python.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+
+pkgs.python311.withPackages (p: with p; [
+  ipython # interactive shell
+  jupyter # interactive notebooks
+  matplotlib # plots
+  numpy # numerical computation
+  pandas # data analysis
+  seaborn
+  # ipykernel
+  # jupyter_client
+
+])
