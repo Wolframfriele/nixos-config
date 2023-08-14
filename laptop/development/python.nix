8c6ed1bb7ab5f770e66ff1e2d74c9c48c2963960
Wolfram Friele <wolframfriele@gmail.com>
2023-08-14 21:49:36 +0200

    Fix way to add base packages to python

--- laptop/development/python.nix
+++ laptop/development/python.nix
@@ -1,13 +1,15 @@
-{ pkgs }:
+{ pkgs, ... }:{
 
-pkgs.python311.withPackages (p: with p; [
-  ipython # interactive shell
-  jupyter # interactive notebooks
-  matplotlib # plots
-  numpy # numerical computation
-  pandas # data analysis
-  seaborn
-  # ipykernel
-  # jupyter_client
-
-])
+  home.packages = [
+    (pkgs.python311.withPackages (p: with p; [
+      ipython # interactive shell
+      jupyter # interactive notebooks
+      matplotlib # plots
+      numpy # numerical computation
+      pandas # data analysis
+      seaborn
+      # ipykernel
+      # jupyter_client
+    ]))
+  ];
+}
