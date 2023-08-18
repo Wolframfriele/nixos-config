265eb6c484278afcac5910136b441c8f86fe60df
Wolfram Friele <wolframfriele@gmail.com>
2023-08-18 22:22:12 +0200

    Set kitty colors

--- laptop/home.nix
+++ laptop/home.nix
@@ -28,22 +28,7 @@
   ];
 
   programs.neovim.enable = true;
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 13;      
-    };
-
-    settings =
-      {
-        scrollback_lines = 10000;
-        enable_audio_bell = false;
-        tab_bar_edge = "top";     # tab bar on top
-        confirm_os_window_close = 0;
-      };
-  };
-
+  
   # services = {                            # Applets
   #   blueman-applet.enable = true;         # Bluetooth
   #   network-manager-applet.enable = true; # Network
